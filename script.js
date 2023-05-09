967146433aa4c0686fe06a1cf9a9848c71389a8e
nesmanpro <nesmanpro@gmail.com>
2023-05-09 18:58:52 +0200

    js doc

---
+++ script.js
@@ -0,0 +1,20 @@
+let optionsButton = document.querySelectorAll(".option-button");
+let advancedButton = document.querySelectorAll(".adv-option-button");
+let fontName = document.getElementById("fontName");
+let fontSizeRef = document.getElementById("fontSize");
+let writingArea = document.getElementById("text-input");
+let linkButton = document.getElementById("createLink");
+let alignButton = document.querySelectorAll(".align");
+let spacingButton = document.querySelectorAll(".spacing");
+let formatButton = document.querySelectorAll(".format");
+let scriptButton = document.querySelectorAll(".script");
+
+let fontList = [
+    "Arial",
+    "Verdana",
+    "Times New Roman",
+    "Garamond",
+    "Geaorgia",
+    "Courier New",
+    "Cursive",
+];
